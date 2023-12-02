7ec9213145b715fe5bfc4b41e470ef60eb66d751
Dennis Koskei <89400168+DennisKoskei@users.noreply.github.com>
2023-12-02 15:44:07 -0800

    Added link to github

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-purple-600 text-white py-4 mt-4">
-            <div className="container mx-auto">
+        <footer className="bg-purple-600 text-white py-8 mt-4">
+            <div className="container mx-auto flex justify-between">
                 <p>&copy; {new Date().getFullYear()} DaisyUI Example </p>
+                <p>
+                    View code on
+                        <Link
+                            href="https://github.com/DennisKoskei/daisyui-example"
+                            className="underline pl-1"
+                        >
+                            Github
+                        </Link>
+                </p>
             </div>
         </footer>
     );
